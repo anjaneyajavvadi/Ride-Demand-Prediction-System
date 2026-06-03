9516228d66a0d532432277ec142e6c6b10bf8245
anjaneyajavvadi <anjaneyajavvadi@gmail.com>
2026-06-03 20:57:26 +0530

    added training loops fpr xgb training"

---
+++ src/training/train.py
@@ -0,0 +1,134 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import polars as pl
+import mlflow
+from config import VALIDATION_SPLIT_DATE,TARGET_COL,MLFLOW_TRACKING_URI,MLFLOW_EXPERIMENT,MODEL_NAME,XGB_PARAMS
+from src.training.evaluate import compute_metrics,plot_predictions
+from src.features.schema import FEATURE_COLUMNS
+import xgboost as xgb
+from sklearn.metrics import root_mean_squared_error
+from pathlib import Path
+
+def load_training_data(df=pl.DataFrame)->tuple:
+
+    train_df=df.filter(pl.col("pickup_hour")<VALIDATION_SPLIT_DATE)
+    val_df=df.filter(pl.col("pickup_hour")>=VALIDATION_SPLIT_DATE)
+
+    zone_hour_avg = (
+        train_df
+        .group_by(
+            ["PULocationID", "hour_of_day"]
+        )
+        .agg(
+            pl.mean("trip_count")
+            .alias("zone_hour_avg")
+        )
+    )
+
+    train_df = train_df.join(
+        zone_hour_avg,
+        on=["PULocationID", "hour_of_day"],
+        how="left"
+    )
+    val_df = val_df.join(
+        zone_hour_avg,
+        on=["PULocationID", "hour_of_day"],
+        how="left"
+    )
+  
+
+    X_train=train_df.select(FEATURE_COLUMNS).to_pandas()
+    y_train=train_df[TARGET_COL].to_pandas()
+    X_val=val_df.select(FEATURE_COLUMNS).to_pandas()
+    y_val=val_df[TARGET_COL].to_pandas()
+
+    X_train['PULocationID']=X_train['PULocationID'].astype("category")
+    X_val["PULocationID"]=X_val["PULocationID"].astype("category")
+
+    return X_train,y_train,X_val,y_val,val_df
+
+def train(df):
+    mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
+    mlflow.set_experiment(MLFLOW_EXPERIMENT)
+
+    X_train,y_train,X_val,y_val,val_df=load_training_data(df)
+
+    with mlflow.start_run():
+        mlflow.set_tags({
+            "model":"XGBoost",
+            "version":"0.1",
+            "name":"ride_analysis_xgboost"
+        })
+
+        mlflow.log_params(
+            XGB_PARAMS
+        )
+
+        model=xgb.XGBRegressor(
+            **XGB_PARAMS,
+            enable_categorical=True,
+            tree_method='hist',
+            early_stopping_rounds=50
+        )
+
+        model.fit(
+            X_train, y_train,
+            eval_set=[(X_val, y_val)],
+            verbose=100,
+        )
+        train_pred = model.predict(X_train)
+        train_metrics = compute_metrics(y_train, train_pred)
+
+        val_pred = model.predict(X_val)
+        val_metrics = compute_metrics(y_val, val_pred)
+
+        
+        save_path=plot_predictions(val_df,val_pred,zone_id=200,save_path=Path("../artifacts/val_forecast.png"),title="validation actual vs prediction plot")
+        train_metrics = {
+            f"train_{k}": v
+            for k, v in train_metrics.items()
+        }
+
+        val_metrics = {
+            f"val_{k}": v
+            for k, v in val_metrics.items()
+        }
+        lag1_rmse=root_mean_squared_error(
+            y_val,
+            X_val["lag_1h"]
+        )
+
+        lag24_rmse=root_mean_squared_error(
+                y_val,
+                X_val["lag_24h"]
+            )
+        lag168_rmse=root_mean_squared_error(
+                y_val,
+                X_val["lag_168h"]
+            )
+        
+        baseline_metrics = {
+        "baseline_lag1_rmse": lag1_rmse,
+        "baseline_lag24_rmse": lag24_rmse,
+            "baseline_lag168_rmse": lag168_rmse,
+        }
+
+        mlflow.log_metrics(baseline_metrics)
+        mlflow.log_metrics(train_metrics)
+        mlflow.log_metrics(val_metrics)
+
+        mlflow.log_artifact(str(save_path))
+
+        mlflow.xgboost.log_model(
+            model,
+            artifact_path="model",
+                registered_model_name="ride_demand_xgb"
+        )
+
+
+
+if __name__=="__main__":
+    train()
+
+
